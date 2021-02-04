f27a26a45e9295744d16f886a20f2a9e1362ac5a
Libor Kučera <developer@ic-servis.com>
2021-02-04 16:31:40 +0100

    Removing Supported platforms

--- Package.swift
+++ Package.swift
@@ -5,9 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "ModalPresentation",
-    platforms: [
-        .iOS("13.0"),
-    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
